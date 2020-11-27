fe98cc3b1b6e2d323a8698872c5e99b798a9eaaa
Daniel Yoo <jarkassp2@gmail.com>
2020-11-27 00:49:41 -0500

    Calculation Part

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -3,12 +3,15 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +20,7 @@ import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
     final int[][] PRICE = { {25, 17, 12}, {25, 18, 14}, {23, 18, 18},{23, 18, 18} };
+    final double NY_TAX = 8.875;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -102,14 +106,49 @@ public class MainActivity2 extends AppCompatActivity {
         tickets.add(3+"");
         tickets.add(4+"");
         tickets.add(5+"");
-        tickets.add(6+"");
-        tickets.add(7+"");
-        tickets.add(8+"");
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, tickets);
         arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         sp.setAdapter(arrayAdapter);
         sp2.setAdapter(arrayAdapter);
         sp3.setAdapter(arrayAdapter);
+        AdapterView.OnItemSelectedListener oc = new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                EditText rawPrice = (EditText) findViewById(R.id.rawTicketPrice);
+
+                EditText salesTax = (EditText) findViewById(R.id.salesTax);
+
+                EditText totalPrice = (EditText) findViewById(R.id.totalPrice);
+
+                Spinner sp = (Spinner) findViewById(R.id.spinner);
+                Spinner sp2 = (Spinner) findViewById(R.id.spinner2);
+                Spinner sp3 = (Spinner) findViewById(R.id.spinner3);
+                Toast toast = Toast.makeText(getApplicationContext(), "Reason can not be blank", Toast.LENGTH_LONG);
+                toast.setMargin(50, 100);
+                toast.show();
+                sp.getSelectedItem();
+                int adultCount = Integer.parseInt(sp.getSelectedItem()+"");
+                int seniorCount = Integer.parseInt(sp2.getSelectedItem()+"");
+                int studentCount = Integer.parseInt(sp3.getSelectedItem()+"");
+                int selectedMuseum = ((Museum) getApplication()).getSelectedMuseum();
+                int ticketPrice = PRICE[selectedMuseum][0] * adultCount +
+                        PRICE[selectedMuseum][1] * seniorCount +
+                        PRICE[selectedMuseum][2] * studentCount;
+
+                rawPrice.setText("ticket price : " + ticketPrice);
+                salesTax.setText("sales tax : " + String.format("%.2f",ticketPrice * NY_TAX / 100));
+                totalPrice.setText("ticket total : " + String.format("%.2f",ticketPrice * (NY_TAX / 100 + 1)));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+        sp.setOnItemSelectedListener(oc);
+        sp2.setOnItemSelectedListener(oc);
+        sp3.setOnItemSelectedListener(oc);
     }
 
     public void goWebsite(View view) {
